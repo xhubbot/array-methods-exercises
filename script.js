919a878b797f60f600200588f71ec7ba5bf6f3e0
Daniil <daniil.kahri@gmail.com>
2025-05-16 20:34:31 +0300

    Calculator function created

--- script.js
+++ script.js
@@ -38,6 +38,49 @@ function camelizeV3(str) {
     .join('');
 }
 
-console.log(camelize('hello-world-i-am-xhubbot'));
-console.log(camelizeV2('hello-world-i-am-xhubbot'));
-console.log(camelizeV3('hello-world-i-am-xhubbot'));
+function filterRange(arr, a, b) {
+  return arr.filter(item => (a <= item && item <= b));
+}
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+function sortDescending(arr) {
+  arr.sort((a, b) => b - a);
+}
+
+function Calculator() {
+  this.methods = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+  };
+
+  this.calculate = function(str) {
+    let split = str.split(' '),
+      a = +split[0],
+      op = split[1],
+      b = +split[2];
+    
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a, b);
+  };
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func;
+  };
+}
+
+let calc = new Calculator;
+
+calc.addMethod("/", (a, b) => a / b);
+alert(calc.calculate("10 / 2"));
